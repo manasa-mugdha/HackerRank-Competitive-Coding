e539aa3074f0150d656ad8e9f7ce759c24d07be0
manasa-mugdha <124026719+manasa-mugdha@users.noreply.github.com>
2023-04-11 00:13:19 +0530

    Create Z 401 The Permutation Problem.c

---
+++ Z 401 The Permutation Problem.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n && k; i++) {
+        int maxIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+        if (maxIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+            k--;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
