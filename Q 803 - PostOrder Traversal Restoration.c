476a73d914b3a9eda18e3c3e4def8e7ac469d375
manasa-mugdha <124026719+manasa-mugdha@users.noreply.github.com>
2023-04-11 00:10:35 +0530

    Update Q 803 - PostOrder Traversal Restoration.c

--- Q 803 - PostOrder Traversal Restoration.c
+++ Q 803 - PostOrder Traversal Restoration.c
@@ -12,3 +12,26 @@ return i;
 }
 return -1;
 }
+void printPostOrder(char in[], char pre[], int n)
+{
+int root = search(in, pre[0], n);
+if (root != 0)
+{
+printPostOrder(in, pre + 1, root);
+}
+if (root != n - 1)
+{
+printPostOrder(in + root + 1, pre + root + 1, n - root - 1);
+}
+printf("%c", pre[0]);
+}
+int main()
+{
+char pre[100],in[100];
+scanf("%s",in);
+scanf("%s",pre);
+int n=strlen(in);
+printPostOrder(in, pre, n);
+return 0;
+}
+
